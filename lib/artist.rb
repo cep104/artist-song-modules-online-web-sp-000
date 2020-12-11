f7c9767f5f2b07d8776f7bf796b70a207c100f55
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-11 23:59:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,6 +2,9 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+
   @@artists = []
 
   def initialize
@@ -9,14 +12,18 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@artists
   end
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 08ecfeee503391ac7178aa132191e2d640f6c62e
   def add_song(song)
     @songs << song
     song.artist = self unless song.artist
@@ -26,4 +33,10 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
+<<<<<<< HEAD
+=======
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
+>>>>>>> 08ecfeee503391ac7178aa132191e2d640f6c62e
 end
