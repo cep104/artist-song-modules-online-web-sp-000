208108b38d734d2bffebb41b1e003ad5827606ed
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-12 00:22:45 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ require_relative '../lib/concerns/paramable'
 require_relative '../lib/artist'
 require_relative '../lib/song'
 
-require_relative '../lib/concerns/paramable'
-
-require_relative '../lib/artist.rb'
-require_relative '../lib/song.rb'
+# require_relative '../lib/concerns/paramable'
+# 
+# require_relative '../lib/artist.rb'
+# require_relative '../lib/song.rb'
